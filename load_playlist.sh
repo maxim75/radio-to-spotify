56db00db4ef4493f77ec58c94ca4a3d3c7fc1f6c
Maksym Kozlenko <max@kozlenko.info>
2025-10-06 19:27:32 +1100

    added flask

--- load_playlist.sh
+++ load_playlist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #source /root/.bashrc
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 which python
 cd /app
 pip install -r /requirements.txt
